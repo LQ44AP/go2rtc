a74d323bd114ce86dae3c147cb46383cf87c4e3d
99010 <11658987+lfznnzq@users.noreply.github.com>
2026-07-31 02:19:09 +0800

    Delete internal directory

--- internal/alsa/alsa.go
+++
@@ -1,7 +0,0 @@
-//go:build !(linux && (386 || amd64 || arm || arm64 || mipsle))
-
-package alsa
-
-func Init() {
-	// not supported
-}
--- internal/debug/README.md
+++
@@ -1,3 +0,0 @@
-# Debug
-
-This module provides `GET /api/stack`, with which you can find hanging goroutines
